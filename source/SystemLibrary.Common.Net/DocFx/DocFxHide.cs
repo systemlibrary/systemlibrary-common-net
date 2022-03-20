f83dfaeb7a81b2cba3e7df450e4b04a67373d8fb
systemlibrary <ceo@systemlibrary.com>
2022-03-21 00:45:15 +0100

    Added class: DocFxHide attribute - can be set on fields, methods, ctors to hide them from the SystemLibrary's DocFx doc generation tool

---
+++ source/SystemLibrary.Common.Net/DocFx/DocFxHide.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SystemLibrary.Common.Net
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Constructor)]
+    public class DocFxHide : Attribute
+    {
+        public DocFxHide()
+        {
+        }
+    }
+}
