b2aefb119e4f734b8b19c5c1d64baddf072237a7
systemlibrary <ceo@systemlibrary.com>
2022-06-26 23:31:30 +0200

    EnvironmentConfig updated comment with sample code for "name"

--- source/SystemLibrary.Common.Net/EnvironmentConfig/EnvironmentConfig.cs
+++ source/SystemLibrary.Common.Net/EnvironmentConfig/EnvironmentConfig.cs
@@ -52,40 +52,29 @@ namespace SystemLibrary.Common.Net
         /// Note: changing environment name requires shell restart (iisreset for instance)
         /// </summary>
         /// <example>
-        /// IIS Express:
-        /// <code class="language-csharp hljs">
-        /// - if: ASPNETCORE_ENVIRONMENT exists in 'Environment Variables on Windows'
-        ///     return: value as 'name'
-        /// - if: ASNETCORE_ENVIRONMENT exists in web.config
-        ///     return: value as 'name'
-        ///     
-        /// //Configuration Mode in Visual Studio might have an affect, cannot remember, will test and update docs...
-        ///     
-        /// return: "" as 'name', never null
-        /// </code>
-        ///  
         /// Test Explorer
         /// <code class="language-xml hljs">
-        /// if: Running Tests in 'Test Explorer'
-        /// - if: mstest.runsettings contains 'ASPNETCORE_ENVIRONMENT' variable
+        /// if: mstest.runsettings contains 'ASPNETCORE_ENVIRONMENT' variable
         ///     then: sets 'temp environment' as value
-        /// - if: 'temp environment' is set, but no transformations are found
-        ///     then: sets 'temp environment' as value from 'Configuration Mode' in Visual Studio
-        ///
-        /// - else:
-        ///     then: sets 'temp environment' as value from 'Configuration Mode' in Visual Studio
-        /// 
-        /// - if: environmentConfig.json exists
-        ///     - if transformation file exists for 'temp environment' 
-        ///         then: transformation is ran
-        ///     - if: environmentConfig.json contains 'name' property
-        ///         return: value as 'name'
-        /// 
-        /// - if: mstest.runsettings contains 'ASPNETCORE_ENVIRONMENT' variable
-        ///      return: value as 'name'
         ///     
-        /// - if: ASPNETCORE_ENVIRONMENT exists in 'Environment Variables on Windows'
-        ///     return: value as 'name'
+        ///     if: 'temp environment' is set, but no transformation is found
+        ///         then: sets 'temp environment' as value from 'Configuration Mode' in Visual Studio
+        ///
+        /// else:
+        ///     then: sets 'temp environment' as value from 'Configuration Mode' in Visual Studio
+        /// 
+        /// if: environmentConfig.json exists
+        ///     if transformation file exists for 'temp environment' 
+        ///         then: run transformation
+        ///     
+        ///     if: environmentConfig.json contains 'name' property
+        ///         return: 'value'
+        /// 
+        /// if: mstest.runsettings contains 'ASPNETCORE_ENVIRONMENT' variable
+        ///      return: 'value'
+        ///     
+        /// if: ASPNETCORE_ENVIRONMENT exists in 'Environment Variables on Windows'
+        ///     return: 'value'
         /// 
         /// return: "" as 'name', never null
         /// </code>
@@ -93,18 +82,18 @@ namespace SystemLibrary.Common.Net
         /// Console Application
         /// <code class="language-xml hljs">
         /// if: environmentConfig.json do not exists:
-        /// - if: mstest.runsettings contains 'ASPNETCORE_ENVIRONMENT' variable
-        ///      return: value as 'name'
+        ///     if: mstest.runsettings contains 'ASPNETCORE_ENVIRONMENT' variable
+        ///         return: 'value'
         ///     
-        /// - if: ASPNETCORE_ENVIRONMENT exists in 'Environment Variables on Windows'
-        ///     return: value as 'name'
+        ///     if: ASPNETCORE_ENVIRONMENT exists in 'Environment Variables on Windows'
+        ///         return: 'value'
         ///     
         /// else if: 
         ///     if: environmentConfig has transformation equal to 'configuration' pass in as argument
-        ///         then: transformation is ran
+        ///         then: run transformation
         ///         
         ///     if: environmentConfig has property 'name'
-        ///         return: value as 'name'
+        ///         return: 'value'
         /// 
         /// return: "" as 'name', never null
         /// </code>
@@ -112,29 +101,136 @@ namespace SystemLibrary.Common.Net
         /// DOTNET TEST 'csproj' --configuration 'release|debug|etc..' command
         /// <code class="language-csharp hljs">
         /// if: environmentConfig.json do not exists:
-        /// - if: mstest.runsettings contains 'ASPNETCORE_ENVIRONMENT' variable
-        ///      return: value as 'name'
+        ///     if: mstest.runsettings contains 'ASPNETCORE_ENVIRONMENT' variable
+        ///         return: 'value'
         ///     
-        /// - if: ASPNETCORE_ENVIRONMENT exists in 'Environment Variables on Windows'
-        ///     return: value as 'name'
+        /// if: ASPNETCORE_ENVIRONMENT exists in 'Environment Variables on Windows'
+        ///     return: 'value'
         ///     
         /// else if: 
         ///     if: environmentConfig has transformation equal to 'configuration' pass in as argument
-        ///         then: transformation is ran
+        ///         then: run transformation
         ///         
         ///     if: environmentConfig has property 'name'
-        ///         return: value as 'name'
+        ///         return: 'value'
         /// 
+        /// return: "" as 'name', never null
+        /// </code>
+        /// 
+        /// IISExpress
+        /// <code class="language-xml hljs">
+        /// if: launchSettings.json exists
+        ///     if: command "IISExpress" exists and contains environment variable 'ASPNETCORE_ENVIRONMENT'
+        ///         if: 'ASPNETCORE_ENVIRONMENT' exists
+        ///             if: environmentConfig.json exists
+        ///                 if: transformation file exists for 'value'
+        ///                 then: run transformation
+        ///                     
+        ///                 if: environmentConfig contains 'name' property  
+        ///                 return: 'value'
+        ///                 
+        ///             return: 'value'
+        ///                 
+        /// if: 'ASPNETCORE_ENVIRONMENT' exists in web.config
+        ///     if: environmentConfig.json exists
+        ///         if: transformation file exists for 'value'
+        ///         then: run transformation
+        ///         
+        ///         if: environmentConfig contains 'name' property
+        ///             return: 'value'
+        ///             
+        ///     return: 'value'
+        ///         
+        /// if: 'ASPNETCORE_ENVIRONMENT' exists as a 'environment variable' in Windows
+        ///     if: environmentConfig.json exists
+        ///         if: transformation file exists for 'value'
+        ///         then: run transformation
+        ///         
+        ///         if: environmentConfig contains 'name' property
+        ///             return: 'value'
+        ///             
+        ///     return: 'value'
+        ///     
+        /// if: launchSettings.json exists
+        ///     if: "iisSettings" contains "iisExpress" and contains environment variable 'ASPNETCORE_ENVIRONMENT'
+        ///         if: environmentConfig.json exists
+        ///             if: transformation file exist for 'value'
+        ///             then: run transformation
+        ///                 
+        ///             if: environmentConfig.json contains 'name' property
+        ///                 return: 'value'
+        ///                         
+        ///         return: 'value'
+        ///         
+        ///     if: "iisSettings" contains environment variable 'ASPNETCORE_ENVIRONMENT'
+        ///         if: environmentConfig.json exists
+        ///             if: transformation file exists for 'value'
+        ///             then: run transformation
+        ///                     
+        ///             if: environmentConfig.json contains 'name' property
+        ///             return: 'value'
+        ///                 
+        ///         return: 'value'
+        ///          
+        ///     if: command "IIS" exists and contains environment variable 'ASPNETCORE_ENVIRONMENT'
+        ///         if: environmentConfig.json exists
+        ///             if: transformation file exists for 'value'
+        ///             then: run transformation
+        ///                 
+        ///             if: environmentConfig.json contains 'name' property
+        ///                 return: 'value'
+        ///                 
+        /// if: environmentConfig.json exists
+        ///     if: environmentConfig.json contains 'name' property
+        ///         return: 'value'
+        ///                         
         /// return: "" as 'name', never null
         /// </code>
         /// 
         /// IIS
         /// <code class="language-xml hljs">
-        /// if: ASPNETCORE_ENVIRONMENT exists in web.config
-        ///     return: value as 'name'
+        /// if: 'ASPNETCORE_ENVIRONMENT' exists in web.config
+        ///     if: environmentConfig.json exists
+        ///         if: transformation file exists for 'value' 
+        ///         then:run transformation
+        ///         
+        ///         if: environmentConfig.json contains 'name' property
+        ///             return: 'value'
+        ///         
+        ///     return: 'value'
         ///     
-        /// //Configuration Mode in Visual Studio might have an affect, cannot remember, will test and update docs...
-        ///     
+        /// if: launchSettings.json exists
+        ///     if: "iisSettings" contains "iisExpress"
+        ///         if: "iisExpress" contains 'environmentVariables'
+        ///             if: 'ASPNETCORE_ENVIRONMENT' exists
+        ///                 if: environmentConfig.json exists
+        ///                     if: transformation file exist for 'value'
+        ///                     then: run transformation
+        ///                 
+        ///                     if: environmentConfig.json contains 'name' property
+        ///                         return: 'value'
+        ///                     
+        ///     if: "iisSettings" contains 'environmentVariables'
+        ///         if: 'ASPNETCORE_ENVIRONMENT' exists
+        ///             if: environmentConfig.json exists
+        ///                 if: transformation file exist for 'value'
+        ///                 then: run transformation
+        ///                 
+        ///                 if: environmentConfig.json contains 'name' property
+        ///                     return: 'value'
+        ///                     
+        ///     if: "profiles" exists
+        ///         if: command "IIS" exists and contains 'environmentVariables'
+        ///             if: 'ASPNETCORE_ENVIRONMENT' exists
+        ///                 if: environmentConfig.json exists
+        ///                     if: trnasformation file exists for 'value'
+        ///                     then: transformation is ran
+        ///                 
+        ///                     if: environmentConfig.json contains 'name' property
+        ///                         return: 'value'
+        ///                         
+        ///                 return: 'value'
+        ///                 
         /// return: "" as 'name', never null
         /// </code>
         /// </example>
